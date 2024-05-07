95e57772df50390e1d7cdfed3de1c7b16aca5a48
Esraa Ahmed <esraaeliba@gmail.com>
2024-05-07 23:29:23 +0300

    restructure folders and files

--- lab1/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
-
-createApp(App).mount('#app')
